3eabf37bfe61fa7ecbaeeffd3feb022557d93a20
ADorian94 <igigaberos@gmail.com>
2021-09-26 17:38:38 +0200

    Create HomeVM.cs

---
+++ TrainingManager/TrainingManager/ViewModel/WorkoutManager/Home/HomeVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrainingManager.ViewModel.WorkoutManager.Home
+{
+    public class HomeVM : WorkoutManagerBaseVM
+    {
+        public override void RefreshWorkouts(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SaveTodayWorkoutFunctionAsync(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
