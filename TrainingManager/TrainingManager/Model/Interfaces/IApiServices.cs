179aba34df22b5ea2329be900a65f3e613dd0b97
Rmedi72 <rada.medard@gmail.com>
2021-09-28 16:58:54 +0200

    create IApiServices

---
+++ TrainingManager/TrainingManager/Model/Interfaces/IApiServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingManager.Data.DTO;
+
+namespace TrainingManager.Model
+{
+    public interface IApiServices
+    {
+          Task<IEnumerable<WeightWorkoutDTO>> GetWeightWorkoutsAsync();
+    }
+}
