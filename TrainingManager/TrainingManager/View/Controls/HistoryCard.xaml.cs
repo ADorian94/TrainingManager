963b0654b47bfb0488cfa4c88a9276ae99447918
ADorian94 <igigaberos@gmail.com>
2021-10-30 17:42:40 +0200

    Create history card item

---
+++ TrainingManager/TrainingManager/View/Controls/HistoryCard.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingManager.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainingManager.View.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HistoryCard : ContentView
+    {
+        public HistoryCard()
+        {
+            InitializeComponent();
+            OpenedCard.IsVisible = true;
+        }
+
+        public string CloseCardMainLabel
+        {
+            get { return (string)GetValue(CloseCardMainLabelProperty); }
+            set { SetValue(CloseCardMainLabelProperty, value); }
+        }
+
+        public static readonly BindableProperty CloseCardMainLabelProperty =
+            BindableProperty.Create("CloseCardMainLabel", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public string OpenCardFirstLabel
+        {
+            get { return (string)GetValue(OpenCardFirstLabelProperty); }
+            set { SetValue(OpenCardFirstLabelProperty, value); }
+        }
+
+        public static readonly BindableProperty OpenCardFirstLabelProperty =
+            BindableProperty.Create("OpenCardFirstLabel", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public string OpenCardFirstValue
+        {
+            get { return (string)GetValue(OpenCardFirstValueProperty); }
+            set { SetValue(OpenCardFirstValueProperty, value); }
+        }
+
+        public static readonly BindableProperty OpenCardFirstValueProperty =
+            BindableProperty.Create("OpenCardFirstValue", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public static readonly BindableProperty OpenCardSecondValueProperty =
+            BindableProperty.Create("OpenCardSecondValue", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public DelegateCommand ButtonCommand
+        {
+            get { return (DelegateCommand)GetValue(ButtonCommandProperty); }
+            set { SetValue(ButtonCommandProperty, value); }
+        }
+
+        public static readonly BindableProperty ButtonCommandProperty =
+            BindableProperty.Create("ButtonCommand", typeof(DelegateCommand), typeof(HistoryCard), defaultValue: default(DelegateCommand), propertyChanged: OnTapCommandPropertyChanged);
+
+        private static void OnTapCommandPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is HistoryCard headerTemplate && newValue is DelegateCommand command)
+            {
+                headerTemplate.ButtonCommand = command;
+            }
+        }
+
+        public string ButtonCommandParameter
+        {
+            get { return (string)GetValue(ButtonCommandParameterProperty); }
+            set { SetValue(ButtonCommandParameterProperty, value); }
+        }
+
+        public static readonly BindableProperty ButtonCommandParameterProperty =
+            BindableProperty.Create("ButtonCommandParameter", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public bool ButtonVisibility
+        {
+            get { return (bool)GetValue(ButtonVisibilityProperty); }
+            set { SetValue(ButtonVisibilityProperty, value); }
+        }
+
+        public static readonly BindableProperty ButtonVisibilityProperty =
+            BindableProperty.Create("ButtonVisibility", typeof(bool), typeof(HistoryCard), defaultValue: false);
+
+        public string ShortDate
+        {
+            get { return (string)GetValue(ShortDateProperty); }
+            set { SetValue(ShortDateProperty, value); }
+        }
+
+        public static readonly BindableProperty ShortDateProperty =
+            BindableProperty.Create("ShortDate", typeof(string), typeof(HistoryCard), string.Empty);
+
+        public string LongDate
+        {
+            get { return (string)GetValue(LongDateProperty); }
+            set { SetValue(LongDateProperty, value); }
+        }
+
+        public static readonly BindableProperty LongDateProperty =
+            BindableProperty.Create("LongDate", typeof(string), typeof(HistoryCard), string.Empty);
+
+    }
+}
